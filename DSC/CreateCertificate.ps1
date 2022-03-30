c6940094c7ef303438b558b729434c84845c4ac5
tiwari247 <chandraprakashtiwari247@gmail.com>
2022-03-30 15:09:53 +0530

    Add Certificate Script

---
+++ DSC/CreateCertificate.ps1
@@ -0,0 +1,6 @@
+﻿mkdir c:\temp
+cd c:\temp
+Invoke-WebRequest -Uri https://github.com/tiwari247/intrust-arm/blob/main/DSC/WinRMOverHTTPS.zip?raw=true -OutFile .\WinRMOverHTTPS.zip
+Expand-Archive -Path C:\temp\WinRMOverHTTPS.zip -DestinationPath C:\
+cd C:\WinRMOverHTTPS\WinRMOverHTTPS
+.\ConfigureWinRM.ps1 -HostName 10.0.0.4
